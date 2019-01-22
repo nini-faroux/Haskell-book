4b4eca3da4d39f18ef2045026b4720743adf59e3
nini-faroux <nfarrel1@tcd.ie>
2019-01-22 18:15:31 +0000

    chapter 9 solutions

---
+++ src/Chap9/Inter2.hs
@@ -0,0 +1,44 @@
+module Chap9.Inter2 where
+
+import Chap9.Inter1 (myWords)
+
+-- Zipping 
+-- 1
+myZip :: [a] -> [b] -> [(a, b)]
+myZip [] _ = [] 
+myZip _ [] = [] 
+myZip (x:xs) (y:ys) = (x,y) : (myZip xs ys)
+
+-- 2
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith _ [] _ = [] 
+myZipWith _ _ [] = [] 
+myZipWith f (x:xs) (y:ys) = (x `f` y) : (myZipWith f xs ys)
+
+-- 3
+zip' :: [a] -> [b] -> [(a, b)]
+zip' xs ys = myZipWith (,) xs ys
+
+-- Filtering 
+-- 1
+mulsOfThree = [x | x <- [1..30], x `rem` 3 == 0]
+
+-- 2
+calc' = length mulsOfThree
+
+-- 3
+myFilter :: String -> [String]
+myFilter xs = filter (\x -> x /= "a" && x /= "an") $ myWords xs
+
+-- List comprehensions
+mySqr = [x ^ 2 | x <- [1..5]]
+myCube = [x ^ 3 | x <- [1..5]]
+
+-- 1
+tups = [(x, y) | x <- mySqr, y <- myCube]
+
+-- 2
+tups' = [(x, y) | x <- mySqr, y <- myCube, x < 50, y < 50]
+
+-- 3
+calc = length $ tups
