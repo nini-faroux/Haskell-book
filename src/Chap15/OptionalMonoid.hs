f2b5bab8618d71870e7ca7e95738b1b322afbb1a
nini-faroux <nfarrel1@tcd.ie>
2019-02-02 20:52:22 +0000

    chapter 15 solutions

---
+++ src/Chap15/OptionalMonoid.hs
@@ -0,0 +1,46 @@
+module Chap15.OptionalMonoid where
+
+import Test.QuickCheck
+
+data Optional a = 
+    Nada 
+  | Only a 
+  deriving (Eq, Show)
+
+instance Monoid a =>
+        Monoid (Optional a) where 
+  mempty = Nada 
+  mappend = (<>)
+
+instance Semigroup a =>
+        Semigroup (Optional a) where
+  Nada <> x = x 
+  x <> Nada = x 
+  (Only x) <> (Only y) = Only (x <> y)
+
+instance Arbitrary a => 
+        Arbitrary (Optional a) where
+  arbitrary = optGen
+
+optGen :: Arbitrary a => Gen (Optional a) 
+optGen = do
+    a <- arbitrary 
+    frequency [(1, return $ Nada), (3, return $ Only a)]
+
+assoc :: (Eq m, Monoid m) => m -> m -> m -> Bool 
+assoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+leftId :: (Eq m, Monoid m) => m -> Bool 
+leftId a = a <> mempty == a
+
+rightId :: (Eq m, Monoid m) => m -> Bool
+rightId a = mempty <> a == a
+
+type OptAssoc = Optional String -> Optional String -> Optional String -> Bool 
+type FstId = Optional String -> Bool 
+
+optTest :: IO () 
+optTest = do
+    quickCheck (assoc :: OptAssoc)
+    quickCheck (leftId :: FstId)
+    quickCheck (rightId :: FstId)
