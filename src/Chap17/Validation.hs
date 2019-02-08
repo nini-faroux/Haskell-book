34c050e35f310ba2a7e000d93fcb00938c09d45c
nini-faroux <nfarrel1@tcd.ie>
2019-02-08 16:37:04 +0000

    validation applicative instance

---
+++ src/Chap17/Validation.hs
@@ -0,0 +1,45 @@
+module Chap17.Validation where
+
+import Test.QuickCheck
+import Test.QuickCheck.Classes
+import Test.QuickCheck.Checkers
+
+data Valid e a = 
+    Fail e
+  | Joy a
+  deriving (Eq, Show)
+
+instance Functor (Valid e) where
+  fmap _ (Fail x) = Fail x 
+  fmap f (Joy x) = Joy (f x)
+
+instance Monoid e => 
+    Applicative (Valid e) where
+  pure = Joy
+
+  (Fail f) <*> (Fail x) = Fail (f <> x)
+  (Fail f) <*> _ = Fail f
+  (Joy f) <*> (Joy x) = Joy (f x)
+  (Joy f) <*> (Fail x) = Fail x
+
+instance (Eq e, Eq a) => 
+        EqProp (Valid e a) where
+  (=-=) = eq
+
+instance (Arbitrary e, Arbitrary a) =>
+        Arbitrary (Valid e a) where
+  arbitrary = valGen 
+
+valGen :: (Arbitrary e, Arbitrary a) => Gen (Valid e a) 
+valGen = do
+    e <- arbitrary
+    a <- arbitrary
+    frequency [(1, return $ Fail e), (3, return $ Joy a)]
+
+type SSS = (String, String, String)
+
+trigValid :: Valid SSS SSS
+trigValid = undefined
+
+validChecker :: IO () 
+validChecker = quickBatch (applicative trigValid)
