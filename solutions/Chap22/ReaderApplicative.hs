21b35397ac2d41b501127712f15934d826a44aff
nini-faroux <nfarrel1@tcd.ie>
2019-02-17 12:52:08 +0000

    reader name clash

--- solutions/Chap22/ReaderApplicative.hs
+++ solutions/Chap22/ReaderApplicative.hs
@@ -2,15 +2,15 @@
 
 module Chap22.ReaderApplicative where
 
-newtype Reader r a = Reader { runReader :: r -> a }
+newtype Reader' r a = Reader' { runReader' :: r -> a }
 
-instance Functor (Reader r) where 
-  fmap :: (a -> b) -> Reader r a -> Reader r b
-  fmap f (Reader ra) = Reader $ \r -> f $ ra r
+instance Functor (Reader' r) where 
+  fmap :: (a -> b) -> Reader' r a -> Reader' r b
+  fmap f (Reader' ra) = Reader' $ \r -> f $ ra r
 
-instance Applicative (Reader r) where
-  pure :: a -> Reader r a
-  pure x = Reader $ const x
+instance Applicative (Reader' r) where
+  pure :: a -> Reader' r a
+  pure x = Reader' $ const x
 
-  (<*>) :: Reader r (a -> b) -> Reader r a -> Reader r b
-  (Reader rab) <*> (Reader ra) = Reader $ \r -> rab r $ ra r
+  (<*>) :: Reader' r (a -> b) -> Reader' r a -> Reader' r b
+  (Reader' rab) <*> (Reader' ra) = Reader' $ \r -> rab r $ ra r
