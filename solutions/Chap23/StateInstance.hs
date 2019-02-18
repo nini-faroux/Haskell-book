810c93a5c25eff6e92a345dae2969b842db4bcf5
nini-faroux <nfarrel1@tcd.ie>
2019-02-18 16:11:54 +0000

    state instance

--- solutions/Chap23/StateInstance.hs
+++ solutions/Chap23/StateInstance.hs
@@ -8,20 +8,18 @@ instance Functor (State' s) where
   fmap :: (a -> b) -> State' s a -> State' s b
   fmap f (State' g) = State' $ \s -> (f . fst $ g s, snd $ g s)
 
-instance Semigroup s =>
-        Applicative (State' s) where
+instance Applicative (State' s) where
   pure :: a -> State' s a
   pure x = State' $ \s -> (x, s)
 
   (<*>) :: State' s (a -> b) -> State' s a -> State' s b
-  (State' sab) <*> (State' g) = State' $ \s -> ((fst $ sab s) (fst $ g s), snd (sab s) <> snd (g s))
+  (State' sab) <*> (State' g) = State' $ \s -> ((fst $ sab s) (fst $ g s), snd (g s))
 
-instance Semigroup s =>
-        Monad (State' s) where
+instance Monad (State' s) where
   return = pure 
 
   (>>=) :: State' s a -> (a -> State' s b) -> State' s b
-  (State' f) >>= g = State' $ \s -> runState' (g . fst $ f s) (snd (f s) <> s)
+  (State' f) >>= g = State' $ \s -> runState' (g . fst $ f s) (snd $ f s)
 
 -- 1
 get' :: State' s s
