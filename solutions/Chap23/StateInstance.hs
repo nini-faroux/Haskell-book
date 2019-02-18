06ceeb52217cd8ccdb08dc9488ee7fc379c7c44b
nini-faroux <nfarrel1@tcd.ie>
2019-02-18 12:20:41 +0000

    state applicative

--- solutions/Chap23/StateInstance.hs
+++ solutions/Chap23/StateInstance.hs
@@ -7,3 +7,11 @@ newtype State' s a = State' { runState' :: s -> (a, s) }
 instance Functor (State' s) where 
   fmap :: (a -> b) -> State' s a -> State' s b
   fmap f (State' g) = State' $ \s -> (f . fst $ g s, snd $ g s)
+
+instance Semigroup s =>
+        Applicative (State' s) where
+  pure :: a -> State' s a
+  pure x = State' $ \s -> (x, s)
+
+  (<*>) :: State' s (a -> b) -> State' s a -> State' s b
+  (State' sab) <*> (State' g) = State' $ \s -> ((fst $ sab s) (fst $ g s), (snd $ sab s) <> (snd $ g s))
