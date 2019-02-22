163f21cf212cbb45d0b62d321c78e59e5d8de9c3
nini-faroux <nfarrel1@tcd.ie>
2019-02-22 18:14:44 +0000

    EitherT Applicative instance

--- solutions/Chap26/EitherT.hs
+++ solutions/Chap26/EitherT.hs
@@ -9,3 +9,11 @@ instance Functor m =>
         Functor (EitherT e m) where
   fmap :: (a -> b) -> EitherT e m a -> EitherT e m b
   fmap f (EitherT ema) = EitherT $ (fmap . fmap) f ema
+
+instance Applicative m => 
+        Applicative (EitherT e m) where
+  pure :: a -> EitherT e m a 
+  pure x = EitherT . pure $ pure x
+
+  (<*>) :: EitherT e m (a -> b) -> EitherT e m a -> EitherT e m b
+  (EitherT emab) <*> (EitherT ema) = EitherT $ (<*>) <$> emab <*> ema
