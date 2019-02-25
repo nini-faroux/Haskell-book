6c2c8c8796fe56c43a0d636c3d2b66382cbeced0
nini-faroux <nfarrel1@tcd.ie>
2019-02-25 17:57:48 +0000

    trans dc wrap up

---
+++ solutions/Chap26/WrapUp.hs
@@ -0,0 +1,28 @@
+module Chap26.WrapUp where
+
+import Control.Monad.Trans.Except 
+import Control.Monad.Trans.Maybe 
+import Control.Monad.Trans.Reader 
+
+-- 1
+getEmbedded :: MaybeT (ExceptT String (ReaderT () IO)) Int 
+getEmbedded = MaybeT . ExceptT . ReaderT $ \_ -> return $ fge ()
+
+fge :: b -> Either a (Maybe Int)
+fge = const . Right . Just $ 1
+
+-------------------------------------------------------
+embedded :: MaybeT (ExceptT String (ReaderT () IO)) Int 
+embedded = return 1 
+
+maybeUnwrap :: ExceptT String (ReaderT () IO) (Maybe Int)
+maybeUnwrap = runMaybeT embedded 
+
+eitherUnwrap :: ReaderT () IO (Either String (Maybe Int)) 
+eitherUnwrap = runExceptT maybeUnwrap 
+
+readerUnwrap :: () -> IO (Either String (Maybe Int))
+readerUnwrap = runReaderT eitherUnwrap
+
+unwrap :: IO (Either String (Maybe Int)) 
+unwrap = readerUnwrap ()
