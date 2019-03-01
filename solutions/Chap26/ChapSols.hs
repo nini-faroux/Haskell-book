8b76c820cc351982619236d6e0ffadc83d54d51b
nini-faroux <nfarrel1@tcd.ie>
2019-03-01 23:23:34 +0000

    readerT chap sols

---
+++ solutions/Chap26/ChapSols.hs
@@ -0,0 +1,28 @@
+module Chap26.ChapSols where 
+
+import Chap22.ReaderMonad
+import Chap26.ReaderT
+import Control.Monad.Identity
+import Control.Monad.Trans.State.Lazy
+
+-- 1
+rDec :: Num a => Reader a a 
+rDec = Reader dec
+
+dec :: Num a => a -> a 
+dec x = x - 1
+
+-- 2
+rShow :: Show a => ReaderT a Identity String
+rShow = ReaderT (return . show)
+
+-- 3
+rPrintAndInc :: (Num a, Show a) => ReaderT a IO a
+rPrintAndInc = ReaderT (\r -> putStrLn ("Hi: " ++ show r) >> return (inc r))
+
+inc :: Num a => a -> a 
+inc x = x + 1
+
+-- 4
+sPrintIncAccum :: (Num a, Show a) => StateT a IO String
+sPrintIncAccum = StateT (\s -> return (show s, inc s))
