c53cfb764d7359f534fb0978673a834b42ab58c0
nini-faroux <nfarrel1@tcd.ie>
2019-04-06 18:16:47 +0100

    snd last

--- 99Problems/solutions/PartOne/Sols.hs
+++ 99Problems/solutions/PartOne/Sols.hs
@@ -4,3 +4,12 @@ module PartOne.Sols where
 myLast :: [a] -> Maybe a 
 myLast = foldr (\x xs -> if null xs then Just x else xs) Nothing 
 
+-- 2 Find the last but one element of a list
+sndLast :: [a] -> Maybe a
+sndLast xs 
+  | length xs < 2 = Nothing 
+  | otherwise = Just . last $ init xs
+
+-- unsafe, assuming list size > 1
+sndLast' :: [a] -> a 
+sndLast' = last . init
