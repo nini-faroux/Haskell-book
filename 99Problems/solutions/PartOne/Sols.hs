68dc4f1b047b87c1ad7ddf9ff71ce2941c0040c0
nini-faroux <nfarrel1@tcd.ie>
2019-04-06 18:24:05 +0100

    kth element

--- 99Problems/solutions/PartOne/Sols.hs
+++ 99Problems/solutions/PartOne/Sols.hs
@@ -1,10 +1,12 @@
 module PartOne.Sols where 
 
--- 1 Find the last element of a list
+-- 1 
+-- Find the last element of a list
 myLast :: [a] -> Maybe a 
 myLast = foldr (\x xs -> if null xs then Just x else xs) Nothing 
 
--- 2 Find the last but one element of a list
+-- 2 
+-- Find the last but one element of a list
 sndLast :: [a] -> Maybe a
 sndLast xs 
   | length xs < 2 = Nothing 
@@ -13,3 +15,10 @@ sndLast xs
 -- unsafe, assuming list size > 1
 sndLast' :: [a] -> a 
 sndLast' = last . init
+
+-- 3 
+-- Find the K'th element of a list. The first element in the list is number 1
+kthElem :: [a] -> Int -> Maybe a 
+kthElem xs n
+  | n > length xs || n < 1 = Nothing 
+  | otherwise              = Just . head $ drop (n-1) xs
