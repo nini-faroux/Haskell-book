fe60726f785c4feeb83d851e07d0b2d6e92a83e2
nini-faroux <nfarrel1@tcd.ie>
2019-04-09 22:22:36 +0100

    bin tree functor, applicative

--- data-structs/structs/Trees/BinaryTree.hs
+++ data-structs/structs/Trees/BinaryTree.hs
@@ -5,6 +5,16 @@ data BinTree a =
   | Node (BinTree a) a (BinTree a)
   deriving Show 
 
+instance Functor BinTree where 
+  fmap _ Leaf = Leaf 
+  fmap f (Node l x r) = Node (f <$> l) (f x) (f <$> r)
+
+instance Applicative BinTree where 
+  pure x = Node Leaf x Leaf 
+  Leaf <*> _ = Leaf 
+  _ <*> Leaf = Leaf 
+  (Node lf f rf) <*> (Node l x r) = Node (lf <*> l) (f x) (rf <*> r)
+
 inOrder :: BinTree a -> [a] 
 inOrder Leaf = []
 inOrder (Node l x r) = inOrder l ++ [x] ++ inOrder r
@@ -16,3 +26,17 @@ preOrder (Node l x r) = [x] ++ preOrder l ++ preOrder r
 postOrder :: BinTree a -> [a] 
 postOrder Leaf = []
 postOrder (Node l x r) = postOrder l ++ postOrder r ++ [x] 
+
+insert :: Ord a => a -> BinTree a -> BinTree a 
+insert x Leaf = Node Leaf x Leaf 
+insert x (Node l y r) 
+  | x >= y    = Node l y (insert x r) 
+  | otherwise = Node (insert x l) y r
+
+testTree :: BinTree Integer 
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+testBinTree :: IO () 
+testBinTree = do 
+    print testTree
+    print $ (*2) <$> testTree
