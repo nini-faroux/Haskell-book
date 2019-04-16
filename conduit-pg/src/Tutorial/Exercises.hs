6e71cdb426bcd3483d0c72a1e6cf89be2958b549
nini-faroux <nfarrel1@tcd.ie>
2019-04-16 22:44:15 +0100

    trans exercise

--- conduit-pg/src/Tutorial/Exercises.hs
+++ conduit-pg/src/Tutorial/Exercises.hs
@@ -2,7 +2,7 @@ module Tutorial.Exercises where
 
 import Conduit
 
--- EXERCISE Rewrite sinkGiven to not use do-notation. Hint: it’ll be easier to go Applicative
+-- 1. EXERCISE Rewrite sinkGiven to not use do-notation. Hint: it’ll be easier to go Applicative
 
 -- solution
 sink :: Monad m => ConduitT Int o m (String, Int) 
@@ -21,3 +21,22 @@ exOne = do
     let resS = runConduitPure $ yieldMany [1..10] .| sink
     print resG
     print resS
+
+-- 2. EXERCISE Modify transGiven so that it does something different for the first 3, second 3, 
+-- and final 3 values from upstream, and drops all other values.
+
+-- solution 
+trans :: Monad m => ConduitT Int Int m () 
+trans = do
+    takeC 3 .| mapC (+ 10) 
+    takeC 3 .| mapC (* 5) 
+    takeC 3 .| mapC (^ 2)
+ 
+-- given
+transGiven :: Monad m => ConduitT Int Int m ()
+transGiven = do
+    takeC 5 .| mapC (+ 1)
+    mapC (* 2)
+
+exTwo :: IO ()
+exTwo = runConduit $ yieldMany [1..10] .| trans .| mapM_C print 
