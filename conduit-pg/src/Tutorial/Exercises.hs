3351e50f9246f57573431712f353bf29b93d7976
nini-faroux <nfarrel1@tcd.ie>
2019-04-16 22:24:13 +0100

    sink exercise

--- conduit-pg/src/Tutorial/Exercises.hs
+++ conduit-pg/src/Tutorial/Exercises.hs
@@ -4,9 +4,11 @@ import Conduit
 
 -- EXERCISE Rewrite sinkGiven to not use do-notation. Hint: it’ll be easier to go Applicative
 
+-- solution
 sink :: Monad m => ConduitT Int o m (String, Int) 
-sink = undefined
+sink = takeC 5 .| mapC show .| foldC >>= \x -> sumC >>= \y -> return (x, y)
 
+-- given
 sinkGiven :: Monad m => ConduitT Int o m (String, Int)
 sinkGiven = do
     x <- takeC 5 .| mapC show .| foldC
@@ -15,5 +17,7 @@ sinkGiven = do
 
 exOne :: IO ()
 exOne = do
-    let res = runConduitPure $ yieldMany [1..10] .| sink
-    print res
+    let resG = runConduitPure $ yieldMany [1..10] .| sinkGiven
+    let resS = runConduitPure $ yieldMany [1..10] .| sink
+    print resG
+    print resS
