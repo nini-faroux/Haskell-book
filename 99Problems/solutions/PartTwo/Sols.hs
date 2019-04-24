b72987324dbd3289228ea84f147a07142b8f15b1
nini-faroux <nfarrel1@tcd.ie>
2019-04-24 23:12:45 +0100

    removeAt

--- 99Problems/solutions/PartTwo/Sols.hs
+++ 99Problems/solutions/PartTwo/Sols.hs
@@ -82,12 +82,23 @@ split xs n = (take n xs, drop n xs)
 
 -- 18
 -- Extract a slice from a list.
--- Given two indices, i and k, the slice is the list containing the elements between the i'th and k'th element of the original list
 -- slice ['a','b','c','d','e','f','g','h','i','k'] 3 7
 -- "cdefg"
 
 slice :: [a] -> Int -> Int -> [a] 
 slice xs i k = take (k-i+1) $ drop (i-1) xs
+
+-- Remove the K'th element from a list.
+-- > removeAt 2 "abcd"
+-- ('b',"acd")
+
+removeAt :: Int -> [a] -> Maybe (a, [a]) 
+removeAt n xs = go xs n 1 []
+  where 
+    go [] _ _ _ = Nothing
+    go (x:xs) n cnt acc 
+      | cnt == n  = Just (x, acc ++ xs)
+      | otherwise = go xs n (cnt+1) (acc ++ [x])
 
 --------------------
 
