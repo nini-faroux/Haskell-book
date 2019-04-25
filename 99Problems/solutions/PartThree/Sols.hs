c1abd3f7636c9d9b60934ca06d8bfd966745e17a
nini-faroux <nfarrel1@tcd.ie>
2019-04-25 01:13:42 +0100

    range

--- 99Problems/solutions/PartThree/Sols.hs
+++ 99Problems/solutions/PartThree/Sols.hs
@@ -5,3 +5,9 @@ module PartThree.Sols where
 -- "aXbcd"
 insertAt :: a -> [a] -> Int -> [a] 
 insertAt x xs k = take (k-1) xs ++ [x] ++ drop (k-1) xs
+
+-- 22
+-- > range 4 9
+-- [4,5,6,7,8,9]
+range :: Int -> Int -> [Int] 
+range s e = [s..e]
