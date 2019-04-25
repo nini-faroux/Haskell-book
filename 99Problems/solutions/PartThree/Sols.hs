b358d51dd72fd622e784a6f801d367706b8d5c82
nini-faroux <nfarrel1@tcd.ie>
2019-04-25 14:58:10 +0100

    rand permutation

--- 99Problems/solutions/PartThree/Sols.hs
+++ 99Problems/solutions/PartThree/Sols.hs
@@ -1,6 +1,7 @@
 module PartThree.Sols where 
 
 import System.Random (randomRIO)
+import Data.Maybe (isNothing)
 
 -- 21 
 -- insertAt 'X' "abcd" 2
@@ -39,3 +40,24 @@ diffSelect n m = go 0 [] (randomRIO (1, m))
       | otherwise = do
           rnd <- rg 
           go (cnt+1) (acc ++ [rnd]) rg
+
+-- 25
+-- Generate a random permutation of the elements of a list.
+-- λ> rnd_permu "abcdef"
+-- "badcef"
+randPermu :: Ord a => [a] -> IO [a] 
+randPermu xs = fmap snd <$> indices xs 
+
+indices :: Ord a => [a] -> IO [(Int, a)] 
+indices xs = go xs (randomRIO (0, length xs - 1)) [] 
+  where 
+    go [] _ acc = return $ qsort acc
+    go li@(x:xs) rg acc = do
+            r <- rg 
+            if isNothing $ lookup r acc 
+               then go xs rg (acc ++ [(r, x)]) 
+               else go li rg acc 
+
+qsort :: Ord a => [a] -> [a] 
+qsort [] = [] 
+qsort (pv:xs) = qsort [x | x <- xs, x < pv] ++ [pv] ++ qsort [x | x <- xs, x >= pv]
