cc7963b1cdf6946482174676a0c4b5b1837e1b3e
nini-faroux <nfarrel1@tcd.ie>
2019-04-25 13:30:34 +0100

    lotto

--- 99Problems/solutions/PartThree/Sols.hs
+++ 99Problems/solutions/PartThree/Sols.hs
@@ -22,7 +22,20 @@ rndSelect :: [a] -> Int -> IO [a]
 rndSelect xs n = go xs 0 [] (randomRIO (0, length xs-1)) 
   where 
     go xs cnt acc rg
-      | cnt == n = return acc 
+      | cnt == n  = return acc 
       | otherwise = do 
            rnd <- rg 
            go xs (cnt+1) (acc ++ [xs !! rnd]) rg
+
+-- 24
+-- Lotto: Draw N different random numbers from the set 1..M.
+-- λ> diff_select 6 49
+-- [23,1,17,33,21,37]
+diffSelect :: Int -> Int -> IO [Int]
+diffSelect n m = go 0 [] (randomRIO (1, m)) 
+  where 
+    go cnt acc rg 
+      | cnt == n  = return acc 
+      | otherwise = do
+          rnd <- rg 
+          go (cnt+1) (acc ++ [rnd]) rg
