6d98d93f7e0844a548b4d4c726d49969b434d52b
nini-faroux <nfarrel1@tcd.ie>
2019-04-25 13:21:42 +0100

    random select

--- 99Problems/solutions/PartThree/Sols.hs
+++ 99Problems/solutions/PartThree/Sols.hs
@@ -1,5 +1,7 @@
 module PartThree.Sols where 
 
+import System.Random (randomRIO)
+
 -- 21 
 -- insertAt 'X' "abcd" 2
 -- "aXbcd"
@@ -11,3 +13,16 @@ insertAt x xs k = take (k-1) xs ++ [x] ++ drop (k-1) xs
 -- [4,5,6,7,8,9]
 range :: Int -> Int -> [Int] 
 range s e = [s..e]
+
+-- 23
+-- Extract a given number of randomly selected elements from a list.
+-- λ> rnd_select "abcdefgh" 3 >>= putStrLn
+-- eda
+rndSelect :: [a] -> Int -> IO [a] 
+rndSelect xs n = go xs 0 [] (randomRIO (0, length xs-1)) 
+  where 
+    go xs cnt acc rg
+      | cnt == n = return acc 
+      | otherwise = do 
+           rnd <- rg 
+           go xs (cnt+1) (acc ++ [xs !! rnd]) rg
