9f41ea3a9239a54305d8efd102360e61a23149d5
nini-faroux <nfarrel1@tcd.ie>
2019-04-26 14:43:37 +0100

    persistent

--- frameworks/spock_/api-one/app/Main.hs
+++ frameworks/spock_/api-one/app/Main.hs
@@ -1,46 +1,63 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE EmptyDataDecls             #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANUGAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
 
 module Main where
 
-import Web.Spock
-import Web.Spock.Config 
+import           Web.Spock
+import           Web.Spock.Config
 
-import Data.Aeson hiding (json)
-import Data.Monoid ((<>))
-import Data.Text (Text, pack)
-import GHC.Generics 
+import           Data.Aeson              hiding (json)
+import           Data.Monoid             ((<>))
+import           Data.Text               (Text, pack)
+import           GHC.Generics
 
-data Person = Person 
-  { name :: Text
-  , age :: Int 
-  } deriving (Generic, Show)
-    
-instance ToJSON Person 
-instance FromJSON Person
+import           Control.Monad.Logger    (LoggingT, runStdoutLoggingT)
+import           Database.Persist        hiding (get)
+import qualified Database.Persist        as P
+import           Database.Persist.Sqlite hiding (get)
+import           Database.Persist.TH
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+Person json 
+  name Text 
+  age Int
+  deriving Show
+|]
 
 type Api = SpockM () () () ()
 type ApiAction a = SpockAction () () () a
 
-app :: Api 
+app :: Api
 app = do
   getPerson
   postPerson
 
 getPerson :: SpockCtxM ctx conn sess st ()
-getPerson = 
-  get "people" $ 
-    json [Person { name = "nini", age = 500 }, Person { name = "major", age = 432}]
+getPerson =
+  get "people" $
+    json 
+      [Person { personName = "nini", personAge = 500 }, 
+       Person { personName = "major", personAge = 432}
+      ]
 
 postPerson :: SpockCtxM () () () () ()
-postPerson = 
+postPerson =
   post "people" $ do
-    pers <- jsonBody' :: ApiAction Person 
+    pers <- jsonBody' :: ApiAction Person
     text $ "Parsed: " <> pack (show pers)
 
 main :: IO ()
 main = do
-  spockCfg <- defaultSpockCfg () PCNoDatabase () 
+  spockCfg <- defaultSpockCfg () PCNoDatabase ()
   runSpock 8080 (spock spockCfg app)
 
 
