5c0c805be2bcc5f54f7d10fb3e8ee2d4934852dc
nini-faroux <nfarrel1@tcd.ie>
2019-04-26 01:16:00 +0100

    spock setup

--- frameworks/spock_/api-one/app/Main.hs
+++ frameworks/spock_/api-one/app/Main.hs
@@ -1,6 +1,33 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric     #-}
+
 module Main where
 
-import Lib
+import Web.Spock
+import Web.Spock.Config 
+
+import Data.Aeson hiding (json)
+import Data.Monoid ((<>))
+import Data.Text (Text, pack)
+import GHC.Generics 
+
+data Person = Person 
+  { name :: Text
+  , age :: Int 
+  } deriving (Generic, Show)
+    
+instance ToJSON Person 
+instance FromJSON Person
+
+type Api = SpockM () () () ()
+type ApiAction a = SpockAction () () () a
+
+app :: Api 
+app = get "people" $ json [Person { name = "nini", age = 400 }, Person { name = "major", age = 232 }]
 
 main :: IO ()
-main = someFunc
+main = do
+  spockCfg <- defaultSpockCfg () PCNoDatabase () 
+  runSpock 8080 (spock spockCfg app)
+
+
