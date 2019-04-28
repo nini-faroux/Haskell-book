3462d69cdac79134d9919f0d3dacfef356089272
nini-faroux <nfarrel1@tcd.ie>
2019-04-28 19:33:49 +0100

    run app

--- frameworks/spock_/api-one/app/Main.hs
+++ frameworks/spock_/api-one/app/Main.hs
@@ -1,19 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module Main where
 
-import           Web.Spock
-import           Web.Spock.Config
-
-import           Control.Monad.Logger    (LoggingT, runStdoutLoggingT)
-import           Database.Persist.Sqlite
-
-import           Api
-import           Models
+import Run
 
 main :: IO ()
-main = do
-  pool <- runStdoutLoggingT $ createSqlitePool "api.db" 5
-  spockCfg <- defaultSpockCfg () (PCPool pool) ()
-  runStdoutLoggingT $ runSqlPool (runMigration migrateAll) pool
-  runSpock 8080 (spock spockCfg app)
+main = runApp
