d8dc41cdec2cb151ca5d793d9d35367700083a6e
nini-faroux <nfarrel1@tcd.ie>
2019-04-29 20:42:54 +0100

    hspec test

--- frameworks/spock_/api-one/test/Spec.hs
+++ frameworks/spock_/api-one/test/Spec.hs
@@ -1,2 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Test.Hspec
+import Test.Hspec.Wai
+import Web.Spock (spockAsApp)
+
+import Run (app)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+spec :: Spec 
+spec = 
+  with (spockAsApp app) $ do
+    describe "GET /" $ 
+      it "is the root route" $
+        get "/" `shouldRespondWith` "Welcome - visit - /users - for list of users"
