6786edcbac87fe95e51c12f3f92c13f2083336c4
nini-faroux <nfarrel1@tcd.ie>
2019-05-03 23:43:56 +0100

    trie quickcheck

--- data-structs/structs/Trees/Trie.hs
+++ data-structs/structs/Trees/Trie.hs
@@ -1,5 +1,9 @@
 module Trees.Trie where 
 
+import Test.QuickCheck
+import Test.QuickCheck.Classes
+import Test.QuickCheck.Checkers
+
 import Data.Maybe (isNothing, fromJust)
 
 data Trie = 
@@ -14,3 +18,16 @@ search (x:_) (TNode b []) = False
 search (x:xs) (TNode b ys) 
   | isNothing $ lookup x ys = False 
   | otherwise = search xs (fromJust $ lookup x ys)
+
+-- testing 
+instance EqProp Trie where 
+  (=-=) = eq 
+
+instance Arbitrary Trie where
+  arbitrary = trieGen
+
+trieGen :: Gen Trie
+trieGen = do
+  b <- (arbitrary :: Gen Bool)
+  c <- arbitrary 
+  oneof [return $ TNode b [], return $ TNode b [c]]
