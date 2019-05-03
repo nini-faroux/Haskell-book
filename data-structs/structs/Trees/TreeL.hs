9483019ee131f9179fba2310b3b831d661aa6ccd
nini-faroux <nfarrel1@tcd.ie>
2019-05-03 11:22:24 +0100

    treeL quickcheck

--- data-structs/structs/Trees/TreeL.hs
+++ data-structs/structs/Trees/TreeL.hs
@@ -2,12 +2,44 @@
 
 module Trees.TreeL where 
 
+import Test.QuickCheck
+import Test.QuickCheck.Classes
+import Test.QuickCheck.Checkers 
+
 data TreeL a = 
   NodeL a [TreeL a] 
   deriving (Eq, Show)
 
 instance Functor TreeL where
-  fmap :: (a -> b) -> TreeL a -> TreeL b
+  fmap f (NodeL x []) = NodeL (f x) []
   fmap f (NodeL x xs) = NodeL (f x) (fmap f <$> xs)
 
+-- testing 
+instance Eq a =>
+        EqProp (TreeL a) where
+  (=-=) = eq
 
+instance Arbitrary a =>
+        Arbitrary (TreeL a) where 
+  arbitrary = treelGen
+
+treelGen :: Arbitrary a => Gen (TreeL a) 
+treelGen = do
+  a <- arbitrary 
+  b <- arbitrary 
+  oneof [return $ NodeL a [], return $ NodeL a [b]]
+
+type SSI = (String, String, Int) 
+
+trigger :: TreeL SSI 
+trigger = undefined 
+
+checkTreeL :: IO () 
+checkTreeL = quickBatch (functor trigger)
+
+treeLMain :: IO () 
+treeLMain = do
+  let t1 = NodeL 1 [NodeL 2 [NodeL 4 [NodeL 8 []]]]
+  print t1
+  print $ (*2) <$> t1
+  sample (treelGen :: Gen (TreeL Int))
