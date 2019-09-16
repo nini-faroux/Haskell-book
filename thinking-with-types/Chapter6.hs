18c7b082eb3f39810a8528f0bc40ab3580b7692c
nini-faroux <nfarrel1@tcd.ie>
2019-09-16 22:13:12 +0100

    rank-n types, chap 6

---
+++ thinking-with-types/Chapter6.hs
@@ -0,0 +1,21 @@
+{- 
+  Exercise 6.3-i
+  What is the rank of:  
+  'Int -> forall a. a -> a' 
+
+  Rank-1, as there 1 arrow after the deepest forall 
+
+  Ex 6.3-ii 
+  What is the rank of:
+  '(a -> b) -> (forall c. c -> a) -> b' 
+
+  Rank-2, 2 arrows after deepest forall
+
+  Ex 6.3-iii 
+  What is the rank of: 
+  '((forall x. mx -> b (z m x) -> b (z m a)) -> m a'
+
+  Rank-3, 3 arrows after deepest forall
+-}
+
+
