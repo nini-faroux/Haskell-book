ce48d70c6dd6a0ba6ed6c318e3c79b242eff6292
nini-faroux <nfarrel1@tcd.ie>
2019-09-24 15:42:05 +0100

    defunc foldr

--- thinking-with-types/Chapter10.hs
+++ thinking-with-types/Chapter10.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE TypeInType             #-}
 {-# LANGUAGE TypeFamilies           #-}
 {-# LANGUAGE TypeOperators          #-}
+{-# LANGUAGE KindSignatures         #-}
 
 import Data.Kind (Constraint, Type)
 
@@ -21,10 +22,10 @@ listToMaybe (x:_) = Just x
 
 newtype ListToMaybe a = ListToMaybe [a]
 
-class Eval l t | l -> t where 
+class Eval' l t | l -> t where 
   eval :: l -> t 
 
-instance Eval (ListToMaybe a) (Maybe a) where 
+instance Eval' (ListToMaybe a) (Maybe a) where 
   eval (ListToMaybe [])    = Nothing 
   eval (ListToMaybe (x:_)) = Just x 
 
@@ -34,35 +35,55 @@ instance Eval (ListToMaybe a) (Maybe a) where
 -}
 
 type Exp a = a -> Type 
-type family Eval' (e :: Exp a) :: a 
+type family Eval (e :: Exp a) :: a 
 
 data ListToMaybe' :: [a] -> Exp (Maybe a)
 
-type instance Eval' (ListToMaybe' '[])      = 'Nothing 
-type instance Eval' (ListToMaybe' (x ': _)) = 'Just x
+type instance Eval (ListToMaybe' '[])      = 'Nothing 
+type instance Eval (ListToMaybe' (x ': _)) = 'Just x
+
+{- 
+  Exercise 10.2-ii
+  Defunctionalize foldr' :: (a -> b -> b) -> b -> [a] -> b 
+-}
+
+foldr' :: (a -> b -> b) -> b -> [a] -> b 
+foldr' _ z [] = z 
+foldr' f z (x:xs) = f x (foldr' f z xs) 
+
+data Foldr :: (a -> b -> Exp b) -> b -> [a] -> Exp b
+
+type instance Eval (Foldr _ z '[]) = z 
+type instance Eval (Foldr f z (x ': xs)) = Eval (f x (Eval (Foldr f z xs)))
 
 {--------------------}
 {- Chapter Examples -}
 
 newtype Fst a b = Fst (a, b)
 
-instance Eval (Fst a b) a where 
+instance Eval' (Fst a b) a where 
   eval (Fst (a, b)) = a 
 
-data MapList dfb a = MapList (a -> dfb) [a] 
+data MapList' dfb a = MapList' (a -> dfb) [a] 
 
-instance Eval dfb dft => 
-    Eval (MapList dfb a) [dft] where 
-  eval (MapList f []) = [] 
-  eval (MapList f (x:xs)) = eval (f x) : eval (MapList f xs)
+instance Eval' dfb dft => 
+    Eval' (MapList' dfb a) [dft] where 
+  eval (MapList' f []) = [] 
+  eval (MapList' f (x:xs)) = eval (f x) : eval (MapList' f xs)
 
 {-- Type-Level --}
 
 -- snd 
 data Snd :: (a, b) -> Exp b 
-type instance Eval' (Snd '(a, b)) = b
+type instance Eval (Snd '(a, b)) = b
 
 -- fromMaybe
 data FromMaybe :: a -> Maybe a -> Exp a 
-type instance Eval' (FromMaybe _1 ('Just a)) = a 
-type instance Eval' (FromMaybe a 'Nothing)   = a 
+type instance Eval (FromMaybe _1 ('Just a)) = a 
+type instance Eval (FromMaybe a 'Nothing)   = a 
+
+-- map 
+data MapList :: (a -> Exp b) -> [a] -> Exp [b]
+
+type instance Eval (MapList f '[]) = '[]
+type instance Eval (MapList f (x ': xs)) = Eval (f x) ': Eval (MapList f xs)
