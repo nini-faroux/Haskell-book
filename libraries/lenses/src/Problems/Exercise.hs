5b38c639e36b76631a7a1596daf59595c45d9c52
nini-faroux <nfarrel1@tcd.ie>
2019-10-17 20:12:52 +0100

    lenses ex 2

--- libraries/lenses/src/Problems/Exercise.hs
+++ libraries/lenses/src/Problems/Exercise.hs
@@ -13,8 +13,6 @@ data Address =
   , _city :: !Text 
   }
 
-makeLenses ''Address 
-
 data Person =
   Person {
     _name :: !Text 
@@ -23,6 +21,22 @@ data Person =
   } 
 
 makeLenses ''Person 
+makeLenses ''Address 
+
+streetL :: Lens' Address Text 
+streetL = lens _street (\addr newSt -> addr { _street = newSt })
+
+cityL :: Lens' Address Text 
+cityL = lens _city (\addr newCity -> addr { _city = newCity })
+
+ageL :: Lens' Person Int 
+ageL = lens _age (\pers newAge -> pers { _age = newAge })
+
+addressL :: Lens' Person Address 
+addressL = lens _address (\pers newAddr -> pers { _address = newAddr })
+
+personStreetL :: Lens' Person Text 
+personStreetL = addressL . streetL 
 
 hollywood :: Text 
 hollywood = "Hollywood Blvd"
@@ -44,6 +58,9 @@ alice = Person
 aliceWilshire :: Person 
 aliceWilshire = setStreet wilshire alice 
 
+aliceWilshireManual :: Person 
+aliceWilshireManual = setStreetManual wilshire alice 
+
 aliceSomeStreet :: Person 
 aliceSomeStreet = setStreet' someStreet alice 
 
@@ -53,12 +70,21 @@ setStreet st = over (address . street) (const st)
 setStreet' :: Text -> Person -> Person 
 setStreet' = set (address . street) 
 
+setStreetManual :: Text -> Person -> Person 
+setStreetManual = set personStreetL 
+
 getStreet :: Person -> Text 
 getStreet person = person^.address.street
+
+getStreetManual :: Person -> Text 
+getStreetManual person = person^.addressL.streetL 
 
 -- increase age by 1 
 birthday :: Person -> Person 
 birthday person = set age (getAge person + 1) person 
+
+birthdayManual :: Person -> Person 
+birthdayManual person = set ageL (getAgeManual person + 1) person
 
 getAge :: Person -> Int 
 getAge = view age 
@@ -66,8 +92,12 @@ getAge = view age
 getAge' :: Person -> Int 
 getAge' person = person^.age 
 
+getAgeManual :: Person -> Int 
+getAgeManual person = person^.ageL 
+
 mainEx1 :: IO () 
 mainEx1 = hspec $ do
+  -- exercise 1 tests 
   it "lives on Wilshire" $ 
     _street (_address aliceWilshire) `shouldBe` wilshire 
   it "lives on some street" $ 
@@ -76,3 +106,10 @@ mainEx1 = hspec $ do
     getStreet alice `shouldBe` hollywood 
   it "birthday" $ 
     getAge (birthday alice) `shouldBe` _age alice + 1
+  -- exercise 2 tests, manual lenses 
+  it "manual lives on wilshire" $ 
+    _street (_address aliceWilshireManual) `shouldBe` wilshire 
+  it "manual getStreet works" $ 
+    getStreetManual alice `shouldBe` hollywood 
+  it "manual birthday" $ 
+    getAgeManual (birthdayManual alice) `shouldBe` _age alice + 1
