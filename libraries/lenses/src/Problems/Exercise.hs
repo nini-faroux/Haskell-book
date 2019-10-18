abd5507741a48021a646cf241252283d72d4a158
nini-faroux <nfarrel1@tcd.ie>
2019-10-18 19:01:20 +0100

    prisms

--- libraries/lenses/src/Problems/Exercise.hs
+++ libraries/lenses/src/Problems/Exercise.hs
@@ -170,3 +170,21 @@ mainEx1 = hspec $ do
         tuple = ((1, 2), (True, 'x', "Hello World"))
      in over tupleLens not tuple `shouldBe`
             ((1, 2), (False, 'x', "Hello World"))
+
+  -- exercise 5, prisms 
+  it "over left on left" $
+    let val :: Either Int Double
+        val = Left 5
+     in over _Left (+ 1) val `shouldBe` Left 6
+  it "over left on right" $
+    let val :: Either Int Double
+        val = Right 5
+     in over _Left (+ 1) val `shouldBe` Right 5
+  it "set left on left" $
+    let val :: Either Int Double
+        val = Left 5
+     in set _Left 6 val `shouldBe` Left 6
+  it "set left on right" $
+    let val :: Either Int Double
+        val = Right 5
+     in set _Left 6 val `shouldBe` Right 5
