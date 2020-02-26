e56e8b4d2a7327b4b2487f6db4eef94f4b6d775e
nini-faroux <nfarrel1@tcd.ie>
2020-02-26 17:33:40 +0000

    type exercise, FlexibleInstances

--- type-exercises/src/FlexibleInstances/Exercises.hs
+++ type-exercises/src/FlexibleInstances/Exercises.hs
@@ -5,30 +5,30 @@ class PopQuiz a
 -- | Which of the following instances require 'FlexibleInstances'? Don't cheat
 -- :D This is a tricky one, but look out for nested concrete types!
 
--- instance PopQuiz Bool
--- instance PopQuiz [Bool]
--- instance PopQuiz [a]
--- instance PopQuiz (a, b)
--- instance PopQuiz [(a, b)]
--- instance PopQuiz (IO a)
+instance PopQuiz Bool
+-- instance PopQuiz [Bool] - Bool isn't a type variable
+instance PopQuiz [a]
+instance PopQuiz (a, b)
+-- instance PopQuiz [(a, b)] -- (,) ins't a type variable 
+instance PopQuiz (IO a)
 
 newtype RIO  r a = RIO (r -> IO a) -- Remember, this is a /new type/.
 type    RIO' r a =      r -> IO a
 
--- instance PopQuiz (RIO Int a)
--- instance PopQuiz (RIO r a)
+-- instance PopQuiz (RIO Int a) -- Int is not a type variable 
+instance PopQuiz (RIO r a)
 -- instance PopQuiz (RIO' r a)
--- instance PopQuiz (r -> IO a)
--- instance PopQuiz (a -> b) -- We can write (a -> b) as ((->) a b).
--- instance PopQuiz (a -> b -> c)
--- instance PopQuiz (a, b, c)
--- instance PopQuiz (a, (b, c))
--- instance PopQuiz ()
--- instance PopQuiz (a, b, c, a)
+-- instance PopQuiz (r -> IO a) -- IO is not a type variable
+instance PopQuiz (a -> b) -- We can write (a -> b) as ((->) a b).
+-- instance PopQuiz (a -> b -> c) -- (->) is not a type variable
+instance PopQuiz (a, b, c)
+-- instance PopQuiz (a, (b, c)) -- (,) is not a type variable
+instance PopQuiz ()
+-- instance PopQuiz (a, b, c, a) -- not unique
 
 data Pair  a = Pair  a  a
 type Pair' a =      (a, a)
 
--- instance PopQuiz (a, a)
--- instance PopQuiz (Pair a)
--- instance PopQuiz (Pair' a)
+-- instance PopQuiz (a, a) -- not unique
+instance PopQuiz (Pair a)
+-- instance PopQuiz (Pair' a) -- not unique
