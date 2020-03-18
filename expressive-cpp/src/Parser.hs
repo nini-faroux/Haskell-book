c6638b73d64c799c55836bb96bd4b8572c0a2955
nini-faroux <nfarrel1@tcd.ie>
2020-03-18 15:06:14 +0000

    parse csv

--- expressive-cpp/src/Parser.hs
+++ expressive-cpp/src/Parser.hs
@@ -2,3 +2,23 @@
 
 module Parser where 
 
+import Control.Applicative ((<|>))
+import qualified Data.Attoparsec.ByteString as P 
+import qualified Data.Attoparsec.ByteString.Char8 as AC
+import qualified Data.Attoparsec.Combinator as PC
+import qualified Data.ByteString as BS
+
+parseAllRecords :: P.Parser [[BS.ByteString]]
+parseAllRecords = AC.many' parseRecord >>= \initRecords -> parseLastRecord >>= \lastRecord -> return $ initRecords ++ [lastRecord]
+
+parseRecord :: P.Parser [BS.ByteString] 
+parseRecord = AC.manyTill parseField (AC.string "\n") 
+
+parseField :: P.Parser BS.ByteString 
+parseField = AC.takeTill (\c -> c == ',' || c == '\r') <* P.take 1
+
+parseLastRecord :: P.Parser [BS.ByteString] 
+parseLastRecord = AC.many' parseField >>= \initFields -> parseLastField >>= \lastField -> return $ initFields ++ [lastField]
+
+parseLastField :: P.Parser BS.ByteString 
+parseLastField = AC.takeTill (== ' ')
