92c68789fb354938d51b33dbb70fe4022a737b0f
nini-faroux <nfarrel1@tcd.ie>
2020-05-02 23:20:50 +0100

    data structs

---
+++ leet/solutions/DataStructures/BinaryTree.hs
@@ -0,0 +1,46 @@
+module DataStructures.BinaryTree where 
+
+data BinaryTree a = 
+    Leaf 
+  | Node a (BinaryTree a) (BinaryTree a) 
+  deriving (Eq, Show)
+
+instance Semigroup a => 
+        Semigroup (BinaryTree a) where 
+  Leaf <> t = t 
+  t <> Leaf = t 
+  (Node x l r) <> (Node y l' r') = Node (x <> y) (l <> l') (r <> r')
+
+instance Monoid a => 
+        Monoid (BinaryTree a) where 
+  mempty = Leaf 
+  mappend = (<>)
+
+instance Functor BinaryTree where 
+  fmap _ Leaf = Leaf 
+  fmap f (Node x l r) = Node (f x) (f <$> l) (f <$> r)
+
+instance Applicative BinaryTree where 
+  pure x = Node x Leaf Leaf
+  Leaf <*> _ = Leaf 
+  _ <*> Leaf = Leaf 
+  (Node f lf rf) <*> (Node x l r) = Node (f x) (lf <*> l) (rf <*> r)
+
+btInsert :: Ord a => a -> BinaryTree a -> BinaryTree a 
+btInsert x Leaf = Node x Leaf Leaf
+btInsert x (Node y l r) 
+  | x <= y = Node y (btInsert x l) r 
+  | otherwise = Node y l (btInsert x r) 
+
+btElem :: Ord a => a -> BinaryTree a -> Bool
+btElem _ Leaf = False 
+btElem x (Node y l r) 
+  | x == y = True 
+  | x < y = btElem x l 
+  | otherwise = btElem x r
+
+foldBinaryTree :: (a -> b -> b) -> b -> BinaryTree a -> BinaryTree b 
+foldBinaryTree f z Leaf = Leaf
+foldBinaryTree f z (Node x l r) = Node x' (foldBinaryTree f x' l) (foldBinaryTree f x' r) 
+  where 
+    x' = f x z
